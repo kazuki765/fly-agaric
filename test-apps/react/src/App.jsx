f1408a403bd37ced8e81b614008469fc3b999444
kazuki765 <kazuki.kujira@gmail.com>
2024-05-25 02:14:12 +0900

    first commit

---
+++ test-apps/react/src/App.jsx
@@ -0,0 +1,13 @@
+import { AutoComplete, Todos } from '@fly-agaric/library-react';
+
+function App() {
+  return (
+    <div>
+      <div>Welcome to React.</div>
+      <AutoComplete />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
